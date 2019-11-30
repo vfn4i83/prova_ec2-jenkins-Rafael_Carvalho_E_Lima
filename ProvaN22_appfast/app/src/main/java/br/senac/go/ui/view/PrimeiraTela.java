16cd521ae8970439e1d3342ebe1b4482d9ba9d1b
rafael <rafaelreg@outlook.com>
2019-11-29 21:55:17 -0300

    atualizando jenkinsfile 4

--- ProvaN22_appfast/app/src/main/java/br/senac/go/ui/view/PrimeiraTela.java
+++ ProvaN22_appfast/app/src/main/java/br/senac/go/ui/view/PrimeiraTela.java
@@ -13,6 +13,5 @@ public class PrimeiraTela extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primeira_tela);
 
-        onCreateView()
     }
 }
