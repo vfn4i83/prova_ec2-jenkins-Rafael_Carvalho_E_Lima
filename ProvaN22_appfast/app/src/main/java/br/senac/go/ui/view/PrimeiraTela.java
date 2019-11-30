2809f300d8471a15a0e49740e72f63ea9743f7ce
rafael <rafaelreg@outlook.com>
2019-11-29 21:15:37 -0300

    upando 1

---
+++ ProvaN22_appfast/app/src/main/java/br/senac/go/ui/view/PrimeiraTela.java
@@ -0,0 +1,18 @@
+package br.senac.go.ui.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.senac.go.R;
+
+public class PrimeiraTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeira_tela);
+
+        onCreateView()
+    }
+}
